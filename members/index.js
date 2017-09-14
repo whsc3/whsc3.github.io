ce2a998865443ba25b97aa665091787ed8994525
saltyJeff <saltyJeff@users.noreply.github.com>
2017-09-14 08:07:47 -0700

    added board to index

--- members/index.js
+++ members/index.js
@@ -1,7 +1,22 @@
 window.members = [
 	{
-		name: "Jefferson Lee",
-		desc: "12th grader at Woodbridge High School",
-		office: "President"
+		"name": "Jefferson Lee",
+		"desc": "12th grader at Woodbridge High School",
+		"office": "President"
+	},
+	{
+		"name": "Aleksander Nelson",
+		"desc": "12th grader at Woodbridge High School",
+		"office": "Vice President"
+	},
+	{
+		"name": "Trisha Nyugen",
+		"desc": "12th grader at Woodbidge High School",
+		"office": "Secretary"
+	},
+	{
+		"name": "Seongsu Park",
+		"desc": "12th grader at Woodbridge High School",
+		"office": "Treasurer"
 	}
 ]
